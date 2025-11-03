7a40c991b3b6aeff7767b6388dbb26b122aaa074
“valentinaottaviani33” <valentinaottaviani33@gmail.com>
2025-11-03 15:41:38 +0100

    primo commit

---
+++ cicli.js
@@ -0,0 +1,174 @@
+//------------------------CICLO FOR---------------------
+
+//Costrutto ciclo FOR ( non possiamo omettere nessuna parte)
+
+//          let i = 0;   -> inizializzazione della variabile ( Contatore )
+//          i < 9;      -> condizione da soddisfare ( se non viene soddisfatta esce da ciclo )
+//          i++;         -> incremento della variabile ( o decremento i-- )
+
+//  0 -> condizione (0 è minore di 10?) si -> procedi e incrementa(o decrementa)
+//  la var diventa 1 perchè ho incrementato la variabile che prima era 0 e ora 1
+//  1 -> condizione (1 è minore di 10?) si -> procedi e incrementa(o decrementa)
+//  la var diventa 2 perchè ho incrementato la variabile che prima era 1 e ora 2
+//  2 -> condizione (2 è minore di 10?) si -> procedi e incrementa(o decrementa)
+//  la var diventa 3 perchè ho incrementato la variabile che prima era 2 e ora 3
+//  ...
+//  la var diventa 10 perchè ho incrementato la variabile che prima era 9 e ora 10
+//  10 è minore di 10? NO , ESCE DAL CICLO
+
+
+
+//Stampo i numeri da 1 a 9
+for ( let i = 0; i < 10; i++){
+
+    //questa parte di codice viene eseguita per ogni iterazione
+    //console.log(i);
+}
+
+//Stampo i numeri da 1 a 10
+for ( let i = 0; i <= 10; i++){
+
+    //console.log(i);
+}
+
+//-------------------------esempi------------------------
+
+//stampare con un ciclo FOR tutti gli elementi di un array
+
+let array = ["Diego", 23, false, "Pippo", true, 45];
+
+
+for(let i = 0; i < array.length; i++){
+
+    //console.log(array[i]);
+}
+
+
+
+let arrayNum = [37, 23, 67, 2, 87, 45];
+
+for(let i = 0; i < arrayNum.length; i++){
+
+    //console.log(arrayNum[i]);
+}
+
+
+
+
+// ------Esercizio completo
+// verrà chiesto all utente per 5 volte un numero;
+// questo numero dovrà esser raddoppiato 
+// stampare i valori raddoppiati
+
+//Creo un array vuoto che verrà popolato dai numeri inseriti
+let numeri = [];
+//Nuovo array con numeri raddoppiati
+let numeriRaddoppiati = [];
+
+//Chiedo i 5 numeri all utente -> quante volte? -> 5 con un ciclo FOR
+for(let i = 0;i < 5; i++){
+
+    let valore = parseInt(prompt("Inserisci un numero (" + (i + 1) + " di 5):"));
+    numeri.push(valore);
+    numeriRaddoppiati.push(numeri[i] * 2);
+}
+
+
+
+//Stampa il risultato
+//console.log("Numeri Originali : " + numeri.join(", "));
+//console.log("Numeri Raddoppiati : " + numeriRaddoppiati.join(", "));
+
+
+//Esercizio 1.
+
+// Chiedo all utente 9 numeri,
+// Il programma verifica se sono pari o dispari,
+// Stampa tutti i risultati ( valori e se pari o dispari)
+
+
+//array per i numeri inseriti
+
+let num = [];
+
+//array per pari o dispari
+
+let risultati = [];
+
+
+//ciclo for 9 volte
+/*
+for (let i = 0; i < 9; i++){
+
+    //chiedo all utente i 9 numeri
+    let valoreInserito = parseInt(prompt("Inserisci un numero (" + (i + 1) + " di 9):"));
+    //ora che ho il numero(valoreInserito), lo pusho dentro all array vuoto "num"
+    num.push(valoreInserito);
+
+    if (valoreInserito % 2 === 0){
+
+        risultati.push("Pari");
+    }else {
+
+        risultati.push("Dispari");
+
+    }
+}
+*/
+
+//stampa risultato
+
+//console.log("Numeri Inseriti : " + num.join(", "));
+//console.log("Numeri Raddoppiati : " + risultati.join(", "));
+
+
+
+
+
+//Esercizio 2.
+
+// generare 5 numeri casuali tra 1 e 50 
+// raddoppiare i valori generati casualmente
+// stampare entrambi i risultati
+
+//array di numeri causuale
+let numeriCasuali = [];
+
+//array numeri raddoppiati
+let numeriDoppi = [];
+
+//per 5 volte
+for(let i = 0;i < 5; i++){
+
+    //genero n casuale
+    let numero = Math.floor(Math.random() * 50) + 1;
+
+    //salvo il numero nell array
+    numeriCasuali.push(numero);
+
+    //calcolo e salvo il doppio
+    numeriDoppi.push(numero * 2);
+
+}
+
+//stampo i risultati
+//console.log("Numeri casuali : " + numeriCasuali.join(", "));
+//console.log("Numeri doppi : " + numeriDoppi.join(", "));
+
+
+
+
+
+//----------------------------------------------------------------CICLO WHILE----------------------------------------------
+
+let numero = 1; //inizializzazione del contatore
+
+while ( numero <= 5 ){ // condizione
+
+    console.log("Numero :", numero);
+    numero++;
+}
+
+console.log("Ciclo terminato");
+
+
